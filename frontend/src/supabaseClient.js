340185d3d0f04ec8d446a1e6784579fc02ebfc2f
sayaliambure <sayaliambure15@gmail.com>
2025-06-24 13:35:25 +0530

    security

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://bbwrdyninlqhrqbaivpq.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJid3JkeW5pbmxxaHJxYmFpdnBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2NzAyOTMsImV4cCI6MjA2NjI0NjI5M30.z9h9KqIj33KUivgFCWzsjyDEyLrIPLixWDjIPed3bKY';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey); 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
